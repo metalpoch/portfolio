9c376610edbb9cf603b4a438bb50ced4c54c7ccd
Keiber Urbila <keiberup.dev@gmail.com>
2023-07-24 09:12:21 -0400

    feat: ✨ Exp view was created

---
+++ src/components/constant/projects.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    href: "https://podcast-t2.vercel.app/",
+    title: "Podcast",
+    body: "Landing Page para el servicio web de podcast para Spotify for Podcasters.",
+  },
+  {
+    href: "https://poch-pptls.vercel.app/",
+    title: "Piedra Papel Tijera Lagarto Spock",
+    body: "Guego inspirado en la serie de televisión The Big Bang Theory.",
+  },
+  {
+    href: "https://github.com/metalpoch/jump-snmp/",
+    title: "Jump SNMP",
+    body: "Realiza peticiones SNMP hacia un servidor remoto mediante SSH utilizando un servidor intermediario..",
+  },
+  {
+    href: "https://github.com/metalpoch/Traffic-Dashboard/",
+    title: "Traffic Dashboard",
+    body: "Visualice la tendencias de las aplicaciones más utilizadas en algunas de las ciudades más importantes de Venezuela.",
+  },
+];
